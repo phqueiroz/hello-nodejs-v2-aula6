f0b183cf493fad418eeaf0c6cea7332ef3ba3c4d
Phillipe de Queiroz <phgqm7@gmail.com>
2016-11-30 21:10:14 -0200

    Exercise 6.2 and 6.3

--- app-web/index.js
+++ app-web/index.js
@@ -1,16 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
 
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
     res.send("Hello world!");
 });
 
-app.get("/dosave", (req, res) => {
+app.post("/dosave", (req, res) => {
     console.log("Os dados do formulario foram recebidos");
-    console.log(req.query);
+    console.log(req.body);
     res.send("<h1><a href='index.html'>Voltar</a></h1>");
+});
+
+app.post("/advinha", (req, res) => {
+    console.log("Numero recebido!");
+    console.log(req.body);
+    res.send("<h1><a href='numero.html'>Voltar</a></h1>");
+});
+
+app.get("/convidados", (req, res) => {
+    let convs = [{nomeconvidado: "Maria"}, {nomeconvidado: "João"}];
+    res.send(convs);
 })
 
 app.listen(3000);
